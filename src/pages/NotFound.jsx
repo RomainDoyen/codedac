149a7213b9dff2dd78a8513db9d80b0a27c1de70
rd39 <romain.doyen97@gmail.com>
2023-01-22 18:35:31 +0400

    Position of the 404 image and fixing of the login drop-down list

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='error'>
       <img src='/images/404-error.png' alt='Page non trouvée'></img>
     </div>
   )
